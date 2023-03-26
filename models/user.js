86ffd01ad6ce7f7da75d7426c86329d0ea5331c8
namhels <8.lsv.888@gmail.com>
2023-03-26 15:58:25 +0300

    add auth

---
+++ models/user.js
@@ -0,0 +1,58 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const emailRegexp =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+const userSchema = Schema(
+  {
+    // name: {
+    //   type: String,
+    //   required: true,
+    // },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      match: emailRegexp,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      // minlength: 6,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const registerUser = Joi.object({
+  // name: Joi.string().required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const loginUser = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const schemas = {
+  registerUser,
+  loginUser,
+};
+
+const User = model("user", userSchema);
+
+module.exports = {
+  User,
+  schemas,
+};
---
+++ routes/api/auth.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const router = express.Router();
+
+const { auth: ctrl } = require("../../controllers");
+
+const { ctrlWrapper } = require("../../helpers");
+
+const { auth, validation } = require("../../middlewares");
+
+const { schemas } = require("../../models/user");
+
+// signup
+router.post(
+  "/register",
+  validation(schemas.registerUser),
+  ctrlWrapper(ctrl.register)
+);
+
+// signin
+router.post("/login", validation(schemas.loginUser), ctrlWrapper(ctrl.login));
+
+router.get("/current", auth, ctrlWrapper(ctrl.getCurrent));
+
+// signout
+router.get("/logout", auth, ctrlWrapper(ctrl.logout));
+
+module.exports = router;
