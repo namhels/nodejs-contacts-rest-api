192e916718ecbe7c791fb4c185c022207b52a9d8
namhels <8.lsv.888@gmail.com>
2023-04-10 13:02:49 +0300

    add updateAvatar

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
 
 const { User } = require("../../models/user");
 
@@ -11,9 +12,11 @@ const register = async (req, res) => {
     throw createError(409, "Email in use");
   }
   const hashPassword = await bcrypt.hash(password, 10);
+  const avatarURL = gravatar.url(email);
   const result = await User.create({
     ...req.body,
     password: hashPassword,
+    avatarURL,
   });
   res.status(201).json({
     user: {
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,7 +6,7 @@ const { auth: ctrl } = require("../../controllers");
 
 const { ctrlWrapper } = require("../../helpers");
 
-const { auth, validation } = require("../../middlewares");
+const { auth, validation, upload } = require("../../middlewares");
 
 const { schemas } = require("../../models/user");
 
@@ -22,14 +22,21 @@ router.post("/login", validation(schemas.loginUser), ctrlWrapper(ctrl.login));
 
 router.get("/current", auth, ctrlWrapper(ctrl.getCurrent));
 
-// signout
-router.get("/logout", auth, ctrlWrapper(ctrl.logout));
-
 router.patch(
   "/",
   auth,
   validation(schemas.updateSubscription),
   ctrlWrapper(ctrl.updateSubscription)
 );
+
+router.patch(
+  "/avatars",
+  auth,
+  upload.single("avatar"),
+  ctrlWrapper(ctrl.updateAvatar)
+);
+
+// signout
+router.get("/logout", auth, ctrlWrapper(ctrl.logout));
 
 module.exports = router;
